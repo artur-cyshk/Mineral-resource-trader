f1d283e4509193c0dee3690bb384e1d6fc1e371e
artur.viachorka <artur.viachorka@itechart-group.com>
2017-08-24 18:44:21 +0300

    some auth logic

--- server/routers/router/routes/authentication/login.js
+++ server/routers/router/routes/authentication/login.js
@@ -4,17 +4,19 @@ const jwt = require("jwt-simple");
 const cfg = require("../../../../configuration/jwt/jwtConfig.js");  
 
 module.exports = function (req, res, next) {
-    if (req.body.name && req.body.password) {
-        var query = "select id, name from user where ? limit 1";
-        connection.query(query, { name: req.body.name, password: req.body.password }, (error, users) => {
-	        if (users && users[0]) {
+    if (req.body.username && req.body.password) {
+         var query = 'select id, name, balance, is_admin as isAdmin from user where name = "' + req.body.username +
+            '" and password_hash = "' + encrypt(req.body.password) + '" and is_email_submitted = 1 and is_banned = 0 limit 1';
+        connection.query(query, (error, users) => {
+        	if (users && users[0]) {
 	            var payload = {
 	                id: users[0].id
 	            };
 	            var token = jwt.encode(payload, cfg.jwtSecret);
 	            res.json({
 	            	message: 'You are welcome!',
-	                token: token
+	                token: token,
+	                user: users[0]
 	            });
 	        }else{
 	        	return next({status: 401, message: "Check your username or password"});
