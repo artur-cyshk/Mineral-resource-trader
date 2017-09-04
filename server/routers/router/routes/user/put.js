186334dc7b5b83ffc6f4407aa12ad3157655e3d5
arturVechorko <school26iko@gmail.com>
2017-09-04 15:17:22 +0300

    admin func && routing fix

--- server/routers/router/routes/user/put.js
+++ server/routers/router/routes/user/put.js
@@ -2,7 +2,7 @@ const connection = require('../../../../configuration/database/connection');
 
 module.exports = function (req, res, next) {
     const query = `update user set ? where id = ?`;
-    connection.query(query, [{ 'is_admin': req.body.isAdmin, 'is_banned' : req.body.isBanned }, req.user.id], (error) => {
+    connection.query(query, [{ 'is_admin': req.body.isAdmin, 'is_banned' : req.body.isBanned }, req.body.id], (error) => {
         if(error) {
             return next({
                 message : 'There was an error while updating user data'
